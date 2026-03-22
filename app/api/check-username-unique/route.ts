e5e5d9b48ad121ef44d53e841528187910f0a794
dhruvu340 <dhruvu3400@gmail.com>
2026-03-22 14:34:52 +0530

    username  unique api

--- app/api/check-username-unique/route.ts
+++ app/api/check-username-unique/route.ts
@@ -9,6 +9,12 @@ const UserNameQuerySchema=z.object({
 })
 
 export async function GET(request:Request){
+    if(request.method!=='GET'){
+         return Response.json({
+                success:false,
+                message:"Method not allowed"
+            },{status:405})
+    }
     await dbConnect();
 
     try {
